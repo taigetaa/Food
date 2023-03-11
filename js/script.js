b51cc7d99706c1b6c4694959228ff71fb4e11023
Victoria <bethmorgendorffer@mail.ru>
2023-03-11 16:39:46 +0300

    Modal

--- js/script.js
+++ js/script.js
@@ -95,12 +95,16 @@ window.addEventListener('DOMContentLoaded', () => {
     const modalTrigger = document.querySelectorAll('[data-modal]'),
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
+
+    function openModal() {
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
-        });
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -121,5 +125,16 @@ window.addEventListener('DOMContentLoaded', () => {
         if (e.code === "Escape" && modal.classList.contains('show')) {
             closeModal();
         }
-    });
+    });    
+
+    const modalTimerId = setTimeout(openModal, 3000);
+
+     function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight + 1 >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });

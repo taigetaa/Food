d26f29266b9b88d5dd66a8f4c0074350376118c4
Victoria <bethmorgendorffer@mail.ru>
2023-04-19 23:51:18 +0300

    axios

--- js/script.js
+++ js/script.js
@@ -188,9 +188,16 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new menuCards(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new menuCards(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
